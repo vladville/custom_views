6ed9501814f08ddd85d435a2af9a779b4d71db2c
Vladimir <vladville@gmail.com>
2025-11-16 21:32:09 +1000

    Init

---
+++ app/src/main/java/ru/netology/statsview/MainActivity.kt
@@ -0,0 +1,28 @@
+package ru.netology.statsview
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import ru.netology.statsview.ui.StatsView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        findViewById<StatsView>(R.id.statsView).data = listOf(
+            0.25F,
+            0.25F,
+            0.25F,
+            0.25F
+        )
+    }
+}
